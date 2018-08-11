a0985ce313358592920148c9eed870d077ab6c22
Richard Orofeo <chardy.orofeo@yahoo.com.ph>
2018-08-11 10:20:00 +0800

    Added concrete implementation for memcachier

--- examples/memcachier/memcachier.go
+++ examples/memcachier/memcachier.go
@@ -1,4 +1,32 @@
 package memcachier
 
-type memcachier struct {
+import "github.com/memcachier/mc"
+
+// Memcachier ...
+type Memcachier struct {
+	server   string
+	username string
+	password string
+}
+
+// NewMemcachier returns instance of memcachier
+func NewMemcachier(server, username, password string) *Memcachier {
+	return &Memcachier{
+		server:   server,
+		username: username,
+		password: password,
+	}
+}
+
+// Set ...
+func (memcachier *Memcachier) Set(key string, data interface{}) (bool, error) {
+	c := mc.NewMC(memcachier.server, memcachier.username, memcachier.password)
+	defer c.Quit()
+	_, err := c.Set(key, data.(string), uint32(0), uint32(0), uint64(0))
+
+	if err != nil {
+		return false, err
+	}
+
+	return true, nil
 }
