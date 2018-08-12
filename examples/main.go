a5125b8750813f1589ee17431baef4f1e0f2a78b
Richard Orofeo <chardy.orofeo@yahoo.com.ph>
2018-08-12 09:08:15 +0800

    Removed reference to memcached in main.go

--- examples/main.go
+++ examples/main.go
@@ -2,25 +2,23 @@ package main
 
 import (
 	"fmt"
-	"log"
-
-	"github.com/whaangbuu/memcacher"
 )
 
 func main() {
-	m := memcacher.NewMemcached("localhost", "11211", "localhost:11211")
+	fmt.Print("Hello, Go!")
+	// m := memcacher.NewMemcached("localhost", "11211", "localhost:11211")
 
-	testKey := "test1"
-	ok, err := m.Set(testKey, "chardy")
+	// testKey := "test1"
+	// ok, err := m.Set(testKey, "chardy")
 
-	if err != nil && !ok {
-		log.Printf("ERROR DUE TO: %v", err)
-	}
+	// if err != nil && !ok {
+	// 	log.Printf("ERROR DUE TO: %v", err)
+	// }
 
-	log.Print(ok)
-	log.Print("Memcache Value")
+	// log.Print(ok)
+	// log.Print("Memcache Value")
 
-	val, _ := memcacher.GetVal(testKey, m)
+	// val, _ := memcacher.GetVal(testKey, m)
 
-	fmt.Print(val)
+	// fmt.Print(val)
 }
