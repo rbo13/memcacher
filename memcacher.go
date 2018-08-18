5f375cace9e5c8aba7ff568a312a6293d4e85d2b
Richard Burk Orofeo <chardy.orofeo@yahoo.com.ph>
2018-08-18 11:55:31 +0800

    Added caching function for interface package
    Signed-off-by: Richard Burk Orofeo <chardy.orofeo@yahoo.com.ph>

--- memcacher.go
+++ memcacher.go
@@ -7,3 +7,15 @@ type Memcacher interface {
 	Get(string) (interface{}, error)
 	Delete(string) (bool, error)
 }
+
+func Set(key string, data interface{}, memcacher Memcacher) (bool, error) {
+	return memcacher.Set(key, data)
+}
+
+func Get(key string, memcacher Memcacher) (interface{}, error) {
+	return memcacher.Get(key)
+}
+
+func Delete(key string, memcacher Memcacher) (bool, error) {
+	return memcacher.Delete(key)
+}
