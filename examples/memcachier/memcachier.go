59adc4be36affca1dfd687b7707eeef5c25310c3
Richard Burk Orofeo <chardy.orofeo@yahoo.com.ph>
2018-08-18 12:26:23 +0800

    Fixed the New function for memcachier
    Signed-off-by: Richard Burk Orofeo <chardy.orofeo@yahoo.com.ph>

--- examples/memcachier/memcachier.go
+++ examples/memcachier/memcachier.go
@@ -4,17 +4,21 @@ import "github.com/memcachier/mc"
 
 // Memcachier ...
 type Memcachier struct {
-	server   string
-	username string
-	password string
+	memcachierClient *mc.Client
+}
+
+// Config configures the memcachier
+// implementation
+type Config struct {
+	Server   string
+	Username string
+	Password string
 }
 
 // NewMemcachier returns instance of memcachier
-func NewMemcachier(server, username, password string) *Memcachier {
+func NewMemcachier(config Config) *Memcachier {
 	return &Memcachier{
-		server:   server,
-		username: username,
-		password: password,
+		memcachierClient: mc.NewMC(config.Server, config.Username, config.Password),
 	}
 }
 
@@ -23,9 +27,7 @@ func NewMemcachier(server, username, password string) *Memcachier {
 // the specified `key`, returns
 // error otherwise.
 func (memcachier *Memcachier) Set(key string, data interface{}) (bool, error) {
-	c := mc.NewMC(memcachier.server, memcachier.username, memcachier.password)
-	defer c.Quit()
-	_, err := c.Set(key, data.(string), uint32(0), uint32(0), uint64(0))
+	_, err := memcachier.memcachierClient.Set(key, data.(string), uint32(0), uint32(0), uint64(0))
 
 	if err != nil {
 		return false, err
@@ -37,10 +39,7 @@ func (memcachier *Memcachier) Set(key string, data interface{}) (bool, error) {
 // Get returns the `data` saved in cache
 // using the specified `key`.
 func (memcachier *Memcachier) Get(key string) (interface{}, error) {
-	c := mc.NewMC(memcachier.server, memcachier.username, memcachier.password)
-	defer c.Quit()
-
-	val, _, _, err := c.Get(key)
+	val, _, _, err := memcachier.memcachierClient.Get(key)
 
 	if err != nil {
 		return nil, err
@@ -53,9 +52,7 @@ func (memcachier *Memcachier) Get(key string) (interface{}, error) {
 // using the specified `key`,
 // returns error otherwise.
 func (memcachier *Memcachier) Delete(key string) (bool, error) {
-	c := mc.NewMC(memcachier.server, memcachier.username, memcachier.password)
-	defer c.Quit()
-	err := c.Del(key)
+	err := memcachier.memcachierClient.Del(key)
 
 	if err != nil {
 		return false, err
