719557458cb411e69bf72cf77249902e279dfc4e
Richard Burk Orofeo <chardy.orofeo@yahoo.com.ph>
2018-08-20 20:45:05 +0800

    Fixed linter and added docs
    Signed-off-by: Richard Burk Orofeo <chardy.orofeo@yahoo.com.ph>

--- memcacher.go
+++ memcacher.go
@@ -8,14 +8,22 @@ type Memcacher interface {
 	Delete(string) (bool, error)
 }
 
+// Set sets data to the cache
+// and returns a boolean value
+// data is saved successfully,
+// returns error otherwise
 func Set(key string, data interface{}, memcacher Memcacher) (bool, error) {
 	return memcacher.Set(key, data)
 }
 
+// Get retrieves data from cache
 func Get(key string, memcacher Memcacher) (interface{}, error) {
 	return memcacher.Get(key)
 }
 
+// Delete deletes an item from the cache
+// using the specified key. Returns
+// boolean value if succcessful, error otherwise
 func Delete(key string, memcacher Memcacher) (bool, error) {
 	return memcacher.Delete(key)
 }
